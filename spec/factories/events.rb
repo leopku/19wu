c58b0631a7b9c05479e8f9974eb8a65220d67e03
leopku <leopku@qq.com>
2013-05-05 19:57:11 +0800

    fixed event factory data to not expired for testing.

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -3,8 +3,8 @@
 FactoryGirl.define do
   factory :event do
     title "19wu development meeting"
-    start_time "2012-12-31 08:00:51"
-    end_time "2012-12-31 09:00:51"
+    start_time "2014-12-31 08:00:51"
+    end_time "2014-12-31 09:00:51"
     location "Tianjin, China"
     content "Contents here"
     slug "rubyconfchina"
